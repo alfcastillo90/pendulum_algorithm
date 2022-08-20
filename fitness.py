6259c47ceb72a3465997ddcabf988e1513b63d74
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2022-08-20 01:41:28 -0400

    ADD: metodo fitness

---
+++ fitness.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import check_solution as CheckSolution
+from cmath import inf
+
+def fitness(setCoveringProblem, sol):
+    cost = setCoveringProblem["coverageCostVector"]
+    feasible = CheckSolution.verify(setCoveringProblem, sol)[0]
+    if not feasible:
+        return inf
+    else:
+        fitness = np.dot(sol, cost)
+        return fitness
